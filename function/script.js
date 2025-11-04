fb71648345ae53f844ce879b47937f554efed8ac
bellaviagabriele03 <bellaviagabriele03@gmail.com>
2025-11-04 12:31:01 +0100

    feat: completato esercizio dei numeri raddoppiati

--- function/script.js
+++ function/script.js
@@ -3,25 +3,64 @@
 // const numeri = [2, 8, 5, 12, 7, 1];
 // console.log(contaMaggiori(numeri, 6)); // 3
 
-
+//EX 1:
 
 //FUNCTION
-function contaMaggiori(number, n1) {
-    let numberMAx = [];
-    for (i = 0; i < number.length; i++) {
-        let curNumber = number[i];
-        if (curNumber > n1) {
-            numberMAx.push(curNumber);
-        }
-    }
-    return numberMAx.length;
-}
+// function contaMaggiori(number, n1) {
+//     let numberMAx = [];
+//     for (i = 0; i < number.length; i++) {
+//         let curNumber = number[i];
+//         if (curNumber > n1) {
+//             numberMAx.push(curNumber);
+//         }
+//     }
+//     return numberMAx.length;
+// }
 
 
 
 
 //dichiaro e assegno un array contenente i numeri
-const numeri = [2, 8, 5, 12, 7, 1, 9, 13, 1];
 
-let result = contaMaggiori(numeri, 6);
+
+
+// const numeri = [2, 8, 5, 12, 7, 1, 9, 13, 1];
+
+// let result = contaMaggiori(numeri, 6);
+// console.log(result);
+
+
+//ES 2:
+
+
+// Scrivi una funzione raddoppia(array) che restituisce un nuovo array in cui ogni numero è il doppio dell’originale.
+
+// const valori = [1, 3, 5, 10];
+// console.log(raddoppia(valori)); // [2, 6, 10, 20]
+
+
+
+function raddoppia(number) {
+    let numberBang = [];
+    for (let i = 0; i < number.length; i++) {
+        let curNumber = number[i];
+        numberBang.push(curNumber + curNumber)
+
+    }
+    return numberBang;
+}
+
+
+
+
+
+const valori = [1, 3, 5, 10];
+
+let result = raddoppia(valori);
 console.log(result);
+
+
+
+
+
+
